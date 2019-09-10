84fa3928f7481ac1a6bf64acf077b21125333773
sand116 <sandsand1166@gmail.com>
2019-09-10 13:45:31 +0900

    folder setting change

--- python_base/array/permutation.py
+++ python_base/array/permutation.py
@@ -1,24 +1,33 @@
 
 
 
-# 순열 조합
-
-def permute(arr):
-    result = [arr[:]]
-    c = [0] * len(arr)
-    i = 0
-    while i < len(arr):
-        if c[i] < i:
-            if i % 2 == 0:
-                arr[0], arr[i] = arr[i], arr[0]
+def permutations(iterable, r=None):
+    # permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC
+    # permutations(range(3)) --> 012 021 102 120 201 210
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r # 예외처리
+    
+    if r > n:
+        return
+    #error 
+    
+    indices = range(n) # 5개를 3개씩으로 순열한다 가정 - 0,1,2,3,4
+    cycles = range(n, n-r, -1) # 5,4,3
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i+1:] + indices[i:i+1]
+                cycles[i] = n - i
             else:
-                arr[c[i]], arr[i] = arr[i], arr[c[i]]
-            result.append(arr[:])
-            c[i] += 1
-            i = 0
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
         else:
-            c[i] = 0
-            i += 1
-    return result
-
+            return
 arr = [1,2,3]
+print(permute(arr)) #함수에 ary 전달 시 단순 복사 
+print (arr) #객체 바뀌어있음 
